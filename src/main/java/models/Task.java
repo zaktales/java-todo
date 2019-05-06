2d7921762ec9d135079ce20ee6f4073be15ffd4e
victor njonge <vinge1718@gmail.com>
2019-05-06 14:48:32 +0300

    added the DAO tests and respective methods

--- src/main/java/models/Task.java
+++ src/main/java/models/Task.java
@@ -2,11 +2,11 @@ package models;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Task {
 
     private String description;
-    private static ArrayList<Task> instances = new ArrayList<>();
     private boolean completed;
     private LocalDateTime createdAt;
     private int id;
@@ -15,20 +15,37 @@ public class Task {
         this.description = description;
         this.completed = false;
         this.createdAt = LocalDateTime.now();
-        instances.add(this);
-        this.id = instances.size();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Task)) return false;
+        Task task = (Task) o;
+        return getCompleted() == task.getCompleted() &&
+                getId() == task.getId() &&
+                Objects.equals(getDescription(), task.getDescription());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getDescription(), getCompleted(), getId());
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getDescription() {
         return description;
-    }
-
-    public static ArrayList<Task> getAll(){
-        return instances;
-    }
-
-    public static void clearAllTasks(){
-        instances.clear();
     }
 
     public boolean getCompleted(){
@@ -43,15 +60,4 @@ public class Task {
         return id;
     }
 
-    public static Task findById(int id){
-        return instances.get(id-1); //why minus 1? See if you can figure it out.
-    }
-
-    public void update(String content) {
-        this.description = content;
-    }
-
-    public void deleteTask(){
-        instances.remove(id-1); //same reason
-    }
 }
